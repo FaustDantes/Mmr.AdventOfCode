5e5d1ed2348f7ea931d7ef9fc2738db641dda5ee
Michal Marcin <michalmarcin2gmail.com>
2025-01-25 15:59:13 +0100

    2024 day 8B solved

--- Mmr.Aoc2024/Days/D8/Day8B.cs
+++ Mmr.Aoc2024/Days/D8/Day8B.cs
@@ -7,17 +7,13 @@ public class Day08B : DayAbstract
     protected override void Runner(Reader reader)
     {
         var inputMap = reader.ReadAsComplex<char>();
-        var debugMap = reader.ReadAsComplex<char>().ToDictionary();
-        
         var calculatedPoints = new HashSet<Complex>();
 
         var antenaGroups = inputMap.Where(kvp => kvp.Value.Value != '.')
             .GroupBy(x => x.Value.Value)
             .Select(g => g.Select(s => s.Value)
                 .ToArray());
-    
-        debugMap.PrintMatrix();
-        
+
         foreach (var antenas in antenaGroups)
         {
             if (antenas.Count() == 1) continue;
@@ -29,48 +25,24 @@ public class Day08B : DayAbstract
                 {
                     var destinationAntena = antenas[destIdx];
                     var delta = sourceAntena.Coordinate - destinationAntena.Coordinate;
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    var antinode = sourceAntena.Coordinate - delta; 
-                    do
-                    {
-                        if (antinode.IsInMatrix(inputMap))
-                        {
-                            calculatedPoints.Add(antinode);
-                            if (debugMap[antinode].Value == '.' )
-                            {
-                                debugMap[antinode].Value = '#';
-                                Console.Clear();
-                                debugMap.PrintMatrix();
-                                Thread.Sleep(500);
-                            }
-                        }
-                        antinode = antinode - delta;
-                    } while (antinode.IsInMatrix(inputMap));
 
-                    antinode = destinationAntena.Coordinate + delta; 
-                    do
+                    var antinode = sourceAntena.Coordinate - delta;
+                    while (antinode.IsInMatrix(inputMap))
                     {
-                        if (antinode.IsInMatrix(inputMap))
-                        {
-                            calculatedPoints.Add(antinode);
-                            if (debugMap[antinode].Value == '.' )
-                            {
-                                debugMap[antinode].Value = '#';
-                                Console.Clear();
-                                debugMap.PrintMatrix();
-                                Thread.Sleep(500);
-                            }
-                        }
+                        calculatedPoints.Add(antinode);
+                        antinode = antinode - delta;
+                    }
+
+                    antinode = destinationAntena.Coordinate + delta;
+                    while (antinode.IsInMatrix(inputMap))
+                    {
+                        calculatedPoints.Add(antinode);
                         antinode = antinode + delta;
-                    } while (antinode.IsInMatrix(inputMap));
+                    }
                 }
 
                 i++;
             }
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Clear();
-            debugMap.PrintMatrix();
-            Thread.Sleep(1000);
         }
 
         Result = calculatedPoints.Count();
