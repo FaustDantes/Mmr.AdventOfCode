20e5b3d89a8cea1ff37110a4e9fdc717a6900a51
Michal Marcin <michalmarcin2gmail.com>
2025-01-26 15:12:52 +0100

    2024 day 9A not optimalized still in progress

--- Mmr.Aoc2024/Days/D9/Day9A.cs
+++ Mmr.Aoc2024/Days/D9/Day9A.cs
@@ -8,20 +8,36 @@ public class Day09A : DayAbstract
 {
     protected override void Runner(Reader reader)
     {
-        var input = reader.ReadAll().ToCharArray(); //.Select(x=> int.Parse(x.ToString())).ToArray();
+        var input = reader.ReadAll().ToCharArray();
         var space = '.';
+        var blocks = string.Concat(input.Where((_, i) => i % 2 == 0)
+            .Select((c, i) =>
+                string.Concat(Enumerable.Repeat(i, int.Parse(c.ToString())))
+                + (i * 2 + 1 < input.Length ? new(space, int.Parse(input[i * 2 + 1].ToString())) : string.Empty))
+        );
 
-        var emptySpace = new List<int>();
-        int index = 0;
-        var blocks = string.Concat( 
-            input.Where((_, i) => i % 2 == 0).Select(
-                (c, i) => new string(char.Parse(i.ToString()), int.Parse(c.ToString())) + (i * 2 + 1 < input.Length ? new (space, int.Parse(input[i * 2 + 1].ToString())) : string.Empty)));
-        
-      // blocks.Select((c, i) => c == space ?  ).ToString();
-        
-        Console.WriteLine(blocks);
-        
-        //Console.WriteLine(Enumerable.Range(0, input.Length).Where(i => i % 2 == 0));
-        Result = 11;
+        var workBlock = blocks.Select(x => x).ToList();
+        var blockIds = new LinkedList<char>(
+            string.Concat(
+                input.Where((_, i) => i % 2 == 0)
+                    .Select((c, i) => string.Concat(Enumerable.Repeat(i, int.Parse(c.ToString()))))
+            ).ToCharArray().Skip(workBlock.Count(c => c == space)).Reverse().ToList()
+        );
+
+        // Console.WriteLine(string.Concat(blockIds));
+        // Console.WriteLine("------------------------------");
+
+        while (workBlock.Contains(space))
+        {
+            var replace =  workBlock.Select((c,i) => (c,i) ).Where(x => x.c == space).First();
+            workBlock[replace.i] = blockIds.First.Value;
+            blockIds.RemoveFirst();
+        }
+
+        var sorted = string.Concat(workBlock.Where(x => char.IsNumber(x)).Select(x => x).ToList());
+        //     Console.WriteLine(sorted);
+
+        var res = workBlock.Where(x => char.IsNumber(x)).Select((c, j) => long.Parse(c.ToString()) * j).Sum();
+        Result = res;
     }
 }
