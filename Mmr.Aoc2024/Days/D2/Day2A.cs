39bd0e589eca8eb5fb9b15911926575894cfb362
ADS\urma11 <michal.marcin@rieter.com>
2025-01-31 15:31:40 +0100

    2024 add input file.txt

--- Mmr.Aoc2024/Days/D2/Day2A.cs
+++ Mmr.Aoc2024/Days/D2/Day2A.cs
@@ -1,9 +1,7 @@
 ﻿namespace Mmr.Aoc2024.Days;
 
-public class Day02A : DayAbstract
-{
-    protected override void Runner(Reader reader)
-    {
+public class Day02A : DayAbstract {
+    protected override void Runner(Reader reader) {
         var levelsPerRow = reader.ReadAndGetLines()
             .Select(line =>
                 line.Split(" ").Select(x => x.Trim()).Where(x => !string.IsNullOrWhiteSpace(x))
@@ -12,48 +10,38 @@ public class Day02A : DayAbstract
 
         var tolerance = 3;
         var safeRows = 0;
-        foreach (var levels in levelsPerRow)
-        {
-          //  Console.WriteLine("levels "+string.Join(" ", levels));
+        foreach (var levels in levelsPerRow) {
             var isSafe = false;
-            if (levels.Count() == 1)
-            {
+            if (levels.Count() == 1) {
                 safeRows++;
                 continue;
             }
 
-            if (Math.Abs(levels[0] - levels[1]) > tolerance) // invalid level
-            {
-                continue;
-            }
+            var invalidLevel = Math.Abs(levels[0] - levels[1]) > tolerance;
+            if (invalidLevel) continue;
 
             if (AreLevelsSafe(levels, tolerance)) safeRows++;
-            // Console.WriteLine("-----------------"+isSafe);
         }
 
         Result = safeRows;
     }
 
-    private static bool AreLevelsSafe(int[] levels, int tolerance)
-    {
+    private static bool AreLevelsSafe(int[] levels, int tolerance) {
         bool isSafe = false;
         var isDecreasing = levels[0] - levels[1] > 0;
-        for (int i = 0; i < levels.Length - 1; i++)
-        {
+        for (int i = 0; i < levels.Length - 1; i++) {
             var isDirectionOk = isDecreasing ? levels[i] - levels[i + 1] : levels[i + 1] - levels[i];
-            if ( isDirectionOk < 0 )
-            {
+            if (isDirectionOk < 0) {
                 isSafe = false;
                 break;
             }
-            // Console.WriteLine("diff "+levels[i] +" - "+  levels[i+1]+ " = "+ Math.Abs(levels[i] - levels[i+1]));
-            var diff = Math.Abs(levels[i] - levels[i+1]);
-            if ( diff > tolerance || diff == 0)
-            {
+            
+            var diff = Math.Abs(levels[i] - levels[i + 1]);
+            if (diff > tolerance || diff == 0) {
                 isSafe = false;
                 break;
-            } 
-                
+            }
+
             isSafe = true;
         }
 
--- Mmr.Aoc2024/Days/D9/Day9A.cs
+++ Mmr.Aoc2024/Days/D9/Day9A.cs
@@ -1,43 +1,49 @@
-﻿using System.Collections.Immutable;
-using System.Drawing;
-using Mmr.Aoc.Common.Models;
+﻿namespace Mmr.Aoc2024.Days;
 
-namespace Mmr.Aoc2024.Days;
+public class Day09A : DayAbstract {
+    protected override void Runner(Reader reader) {
+        string input = reader.ReadAll();
+        var disk = ParseDisk(input);
+        CompactDisk(disk);
+        int checksum = CalculateChecksum(disk);
+        Console.WriteLine("Checksum: " + checksum);
 
-public class Day09A : DayAbstract
-{
-    protected override void Runner(Reader reader)
-    {
-        var input = reader.ReadAll().ToCharArray();
-        var space = '.';
-        var blocks = string.Concat(input.Where((_, i) => i % 2 == 0)
-            .Select((c, i) =>
-                string.Concat(Enumerable.Repeat(i, int.Parse(c.ToString())))
-                + (i * 2 + 1 < input.Length ? new(space, int.Parse(input[i * 2 + 1].ToString())) : string.Empty))
-        );
+        Result = checksum;
+    }
+    static List<char> ParseDisk(string input) {
+        List<char> disk = new();
+        int fileId = 0;
 
-        var workBlock = blocks.Select(x => x).ToList();
-        var blockIds = new LinkedList<char>(
-            string.Concat(
-                input.Where((_, i) => i % 2 == 0)
-                    .Select((c, i) => string.Concat(Enumerable.Repeat(i, int.Parse(c.ToString()))))
-            ).ToCharArray().Skip(workBlock.Count(c => c == space)).Reverse().ToList()
-        );
+        for (int i = 0; i < input.Length; i++) {
+            int length = input[i] - '0';
+            if (i % 2 == 0) {
+                disk.AddRange(Enumerable.Repeat((char)('0' + fileId), length));
+                fileId++;
+                continue;
+            }
 
-        // Console.WriteLine(string.Concat(blockIds));
-        // Console.WriteLine("------------------------------");
-
-        while (workBlock.Contains(space))
-        {
-            var replace =  workBlock.Select((c,i) => (c,i) ).Where(x => x.c == space).First();
-            workBlock[replace.i] = blockIds.First.Value;
-            blockIds.RemoveFirst();
+            disk.AddRange(Enumerable.Repeat('.', length));
         }
+        return disk;
+    }
 
-        var sorted = string.Concat(workBlock.Where(x => char.IsNumber(x)).Select(x => x).ToList());
-        //     Console.WriteLine(sorted);
+    static void CompactDisk(List<char> disk) {
+        int writeIndex = 0;
+        for (int readIndex = 0; readIndex < disk.Count; readIndex++) {
+            if (disk[readIndex] != '.') {
+                (disk[writeIndex], disk[readIndex]) = (disk[readIndex], disk[writeIndex]);
+                writeIndex++;
+            }
+        }
+    }
 
-        var res = workBlock.Where(x => char.IsNumber(x)).Select((c, j) => long.Parse(c.ToString()) * j).Sum();
-        Result = res;
+    static int CalculateChecksum(List<char> disk) {
+        int checksum = 0;
+        for (int i = 0; i < disk.Count; i++) {
+            if (disk[i] != '.') {
+                checksum += i * (disk[i] - '0');
+            }
+        }
+        return checksum;
     }
 }
